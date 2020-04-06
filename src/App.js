cda6ae9c937cbb090b37a64f9052e6d7d5bb5983
GA <ogogoogg@hanmail.net>
2020-04-06 23:01:55 +0900

    initallzation commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { render } from '@testing-library/react';
+
+function App extends Comment {
+  render() {
+    return (
+      <div className="gray-background">
+        <img src={log} lat="log" />
+        <h2>GOGO~~~</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
